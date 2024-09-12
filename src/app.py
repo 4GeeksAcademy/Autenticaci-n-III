a5d6366de6ea4c4e3eb519df333f5c7c27802f42
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-09-12 16:02:54 +0000

    agregados librerias

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ jwt = JWTManager(app)
 @jwt.token_in_blocklist_loader
 def check_if_token_revoked(jwt_header, jwt_payload: dict) -> bool:
     jti = jwt_payload["jti"]
-    token = TokenBlockList.query.filter_by(jti=jti).first()
+    token = TokenBlockedList.query.filter_by(jti=jti).first()
 
     return token is not None
 
